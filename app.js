d96490fe5ae689b42e768cd12bac55a27adb108f
Thomas Chemmanoor <tpc@duke.edu>
2019-08-06 16:04:26 -0400

    added printing post requests to console prototype

--- app.js
+++ app.js
@@ -34,6 +34,11 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+// prints post request body to console
+app.post('/', (req, res) => {
+  console.log(req.body);
+});
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development

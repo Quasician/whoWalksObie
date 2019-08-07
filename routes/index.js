12767c5782fe6375a504658f0992606363c14747
Thomas Chemmanoor <tpc@duke.edu>
2019-08-06 22:10:30 -0400

    Added refresh functionality of webpage. It pulls from DynamoDB on each get request

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'xpress' });
+  const request = require('request');
+  var options = {
+    url: 'https://jeyvj7tvwe.execute-api.us-east-2.amazonaws.com/Gamma/getLastTime',
+    headers: {
+      "x-api-key" : "FVTP4xt1vA5Gb66gkIxn99O8RsfTL8KH9BsSuExG",
+      "Content-Type" : "application/json"
+    } 
+  };
+
+  r = request(options, function (error, response, body) {
+    console.error('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+    res.render('index', { title: body.substring(1,body.length-1)});
+  });
 });
 
 // POST homepage

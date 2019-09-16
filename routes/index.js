f19e34a30e089702cbb68ea1ad5cb83f6a2a3da3
Thomas Chemmanoor <tpc@duke.edu>
2019-09-16 19:34:38 -0400

    Trying to fix lambda error by taking out res.status

--- routes/index.js
+++ routes/index.js
@@ -22,11 +22,36 @@ router.get('/', function(req, res, next) {
  res.render('index', { time: '12:00PM', name:'Thomas', menuId: 'home'});
 });
 
+router.get('/trends', function(req, res, next) {
+  /*const request = require('request');
+  var options = {
+    url: 'https://jeyvj7tvwe.execute-api.us-east-2.amazonaws.com/Gamma/getLastTime',
+    headers: {
+      "x-api-key" : "FVTP4xt1vA5Gb66gkIxn99O8RsfTL8KH9BsSuExG",
+      "Content-Type" : "application/json"
+    } 
+  };
+
+  r = request(options, function (error, response, body) {
+    console.error('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+    res.render('index', { title: body.substring(1,body.length-1)});
+  });
+  */
+ res.render('trends', { time: '12:00PM', name:'Thomas', menuId: 'home'});
+});
+
 // POST homepage
 router.post('/', function(req, res, next){
   console.log(req.body);
   res.render('index', { title: req.body["TimeStamp"]});
   res.status(200).end();
+});
+router.post('/trends', function(req, res, next){
+  console.log(req.body);
+  res.render('trends', { title: req.body["TimeStamp"]});
+  //res.status(200).end();
 });
 
 module.exports = router;
